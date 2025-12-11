5dbd1e768966093453e13e052cf6db30946985f9
Marek-technologie <marekksobanski@gmail.com>
2025-12-11 16:10:59 +0100

    Update menu.cpp

--- Projekt1/menu.cpp
+++ Projekt1/menu.cpp
@@ -3,25 +3,25 @@
 
 Menu::Menu(float width, float height)
 {
-    // Zamiast "fonts/arial.ttf" wpisz:
+    
     if (!m_font.loadFromFile("C:/Windows/Fonts/arial.ttf")) {
         std::cerr << "Blad ladowania czcionki!" << std::endl;
     }
 
-    // ZMIANA: Mamy teraz 3 elementy
+    
     m_items.resize(3);
 
-    // 0: Nowa gra
+    //  Nowa gra
     m_items[0].setFont(m_font);
     m_items[0].setString("Nowa gra");
     m_items[0].setPosition(width / 2 - 60, 200);
 
-    // 1: Wczytaj gre (NOWE)
+    //  Wczytaj gre 
     m_items[1].setFont(m_font);
     m_items[1].setString("Wczytaj gre");
     m_items[1].setPosition(width / 2 - 60, 260);
 
-    // 2: Wyjscie (Przesuniête w dó³)
+    //  Wyjscie 
     m_items[2].setFont(m_font);
     m_items[2].setString("Wyjscie");
     m_items[2].setPosition(width / 2 - 60, 320);
@@ -46,4 +46,5 @@ void Menu::moveDown()
 {
     if (m_selected < m_items.size() - 1)
         m_selected++;
+
 }
